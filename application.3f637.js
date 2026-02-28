d6a5a9fb0109966e3eaa539409aa02707a3434d2
CAM <>
2026-02-28 16:43:35 +0700

    Updates

--- application.3f637.js
+++ application.3f637.js
@@ -16,7 +16,7 @@ System.register([], function (_export, _context) {
         function Application() {
           _classCallCheck(this, Application);
 
-          this.settingsPath = 'src/settings.20a1b.json';
+          this.settingsPath = 'src/settings.acfb0.json';
           this.showFPS = false;
         }
 
--- index.bfbf1.js
+++ index.bfbf1.js
@@ -1,4 +1,4 @@
-System.register(["./application.ae389.js"], function (_export, _context) {
+System.register(["./application.3f637.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
@@ -8,8 +8,8 @@ System.register(["./application.ae389.js"], function (_export, _context) {
   }
 
   return {
-    setters: [function (_applicationAe389Js) {
-      Application = _applicationAe389Js.Application;
+    setters: [function (_application3f637Js) {
+      Application = _application3f637Js.Application;
     }],
     execute: function () {
       canvas = document.getElementById('GameCanvas');
